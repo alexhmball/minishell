b3fd0d1fba77e6d4b8a87143dca5ac0e8c20c40f
Alexandra Ball <alex.ball4000@gmail.com>
2022-12-09 02:38:41 +0400

    fixed outfiles

--- srcs/flag_out.c
+++ srcs/flag_out.c
@@ -6,13 +6,13 @@
 /*   By: aball <aball@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 21:57:01 by aball             #+#    #+#             */
-/*   Updated: 2022/12/08 21:57:07 by aball            ###   ########.fr       */
+/*   Updated: 2022/12/09 02:37:21 by aball            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void	flag_out_2(t_pipe *temp, t_pipe *prev, int i, t_cmd *args)
+static t_pipe	*flag_out_2(t_pipe *temp, t_pipe *prev, int *i, t_cmd *args)
 {
 	temp->append = 1;
 	temp->out = 1;
@@ -22,15 +22,18 @@ static void	flag_out_2(t_pipe *temp, t_pipe *prev, int i, t_cmd *args)
 	{
 		temp->next->out = 1;
 		temp->next->append = 1;
-		temp = remove_node(args->pipe, temp, prev, i);
+		temp = remove_node(args->pipe, temp, prev, *i);
+		*i = 0;
 	}
+	return (temp);
 }
 
-static void	flag_out_1(t_pipe *temp, t_pipe *prev, int *i, t_cmd *args)
+static t_pipe	*flag_out_1(t_pipe *temp, t_pipe *prev, int *i, t_cmd *args)
 {
 	temp->next->out = 1;
 	temp = remove_node(args->pipe, temp, prev, *i);
 	*i = 0;
+	return (temp);
 }
 
 void	flag_out(t_cmd *args)
@@ -46,7 +49,7 @@ void	flag_out(t_cmd *args)
 	{
 		if (temp->cmd[0] && temp->cmd[0][0] == '>'
 				&& ft_strlen(temp->cmd[0]) == 1)
-			flag_out_1(temp, prev, &i, args);
+			temp = flag_out_1(temp, prev, &i, args);
 		else if (temp->cmd[0] && temp->cmd[0][0] == '>'
 				&& temp->cmd[0][1] != '>' && !temp->double_q && !temp->single_q)
 		{
@@ -55,7 +58,7 @@ void	flag_out(t_cmd *args)
 		}
 		else if (temp->cmd[0] && temp->cmd[0][0] == '>'
 			&& temp->cmd[0][1] == '>' && !temp->double_q && !temp->single_q)
-			flag_out_2(temp, prev, i, args);
+			temp = flag_out_2(temp, prev, &i, args);
 		prev = temp;
 		temp = temp->next;
 		i++;
